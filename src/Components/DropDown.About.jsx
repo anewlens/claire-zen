fe630fb63ebcf4338aeee05de23213a2ea288404
anewlens <36173414+anewlens@users.noreply.github.com>
2019-10-03 22:02:50 -0400

    optimized header images

--- src/Components/DropDown.About.jsx
+++ src/Components/DropDown.About.jsx
@@ -7,11 +7,10 @@ const DropDown = () => {
             <p className="About-description">Before moving to New York, Claire obtained a BFA in Graphic Design from Virginia Commonwealth University. She currently works at Money20/20 helping deliver memorable marketing and event imagery. Primarily a 2D designer, she also has experience in fashion and product design. She finds joy in satisfying animated gifs and beautiful, sustainable fashion packaging.</p> 
             <p className="About-description">In her spare time, Claire is designing a Star Wars DnD campaign at home with her cat Ahsoka. When she’s out and about, Claire is hunting for the best Cubano sandwich or looking out for the prettiest cloud.</p>
             <ProgressiveImage
-                className='About-img'
                 placeholder={`${window.location.origin}/images/lazy_Soka Landscape.jpg`} 
                 src={`${window.location.origin}/images/Soka Landscape.jpg`} 
                 alt="Soka">
-                    {src => <img src={src} alt="Soka" />}
+                    {src => <img src={src} alt="Soka" className='About-img' />}
             </ProgressiveImage>
         </div>
     )
--- src/Components/ImageLoader.jsx
+++ src/Components/ImageLoader.jsx
@@ -3,9 +3,9 @@ import ProgressiveImage from 'react-progressive-image'
 
 const ImageLoader = ({className, image}) => (
     <ProgressiveImage
-    placeholder={`${window.location.origin}/images/lazy_${image}`}
-    src={`${window.location.origin}/images/${image}`}>
-        {src => <img src={src} className={className} />}
+        src={`${window.location.origin}/images/web_${image}`}
+        placeholder={`${window.location.origin}/images/lazy_${image}`}>
+        {src => <img src={src} className={className} alt={image}/>}
     </ProgressiveImage>
 )
 
